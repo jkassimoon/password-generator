fc2d10ec1876259f1beca0df528eac43acb551e7
thepickupkitten <jkashmoon@gmail.com>
2020-03-21 22:58:57 -0700

    pseudo coded

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,6 +3,9 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+
+  prompt("test");
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
